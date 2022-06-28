4caf9b57aed346c2a3ce61f995d6279f9fb8e650
lancey1 <lanceyu619@gmail.com>
2022-06-28 16:44:53 -0400

    add 3 more stores and load subset of stores to variables

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,22 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+
+Surrey = Store.create(name: "Surrey", annual_revenue: 224000, mens_apparel: false, womens_apparel: true)
+Whistler = Store.create(name: "Whistler", annual_revenue: 1900000, mens_apparel: true, womens_apparel: false)
+Yaletown = Store.create(name: "Yaletown", annual_revenue: 430000, mens_apparel: true, womens_apparel: false)
+
+@mens_stores = Store.where(mens_apparel: true)
+
+puts @mens_stores
+
+@mens_stores.each{|store| puts store.name, store.annual_revenue}
+
+
+
+# chaining wheres
+@womens_stores = Store.where(womens_apparel: true).and(Store.where(annual_revenue: (0..1000000)))
+# OR ? refers to value after comma
+# @womens_stores = Store.where("womens_apparel = true AND annual_revenue < ?", 1000000)
+
+@womens_stores.each{|store| puts store.name, store.annual_revenue}
